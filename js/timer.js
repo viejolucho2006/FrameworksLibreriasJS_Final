348d518eb38f077adabd35824b23d61a2bd7c15c
luis_lopez <luis_lopez23@hotmail.com>
2020-10-17 21:12:11 -0500

    boton1
    Se agrega al index el id para el boton1 mismo que activara la cuenta regresiva

--- js/timer.js
+++ js/timer.js
@@ -1,17 +1,28 @@
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    var interval= setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+var seconds = 120; //número de segundos a contar
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+window.onload = function(){
+	document.getElementById("boton1").onclick=inicio;
+	conteo=document.getElementById("timer");
+}
 
-        $(display).text(minutes + ":" + seconds);
+function inicio(){ 
+	function conteo() {
+	  var minutes = Math.round((seconds - 30)/60); //calcula el número de minutos
+	  var remainingSeconds = seconds % 60; //calcula los segundos
+	  //si los segundos usan sólo un dígito, añadimos un cero a la izq
+	  if (remainingSeconds < 10) { 
+		remainingSeconds = "0" + remainingSeconds; 
+	  } 
+	  document.getElementById('timer').innerHTML = "0"+minutes + ":" +     remainingSeconds; 
+	  if (seconds == 0) { 
+		clearInterval(countdownTimer); 
+		$('div.panel-tablero, div.time').hide();
+		$('h1.main-titulo').addClass('title-over').text('Gracias por jugar!');
+		$('div.score, div.moves, div.panel-score').width('100%');
+	  } else { 
+		seconds--; 
+	  } 
+	} 
 
-        if (--timer < 0) {
-            $("body").trigger("finTiempo");
-						clearInterval(interval)
-        }
-    }, 1000);
+	var countdownTimer = setInterval(conteo, 1000); //actualizar la vista de la cuenta regresiva
 }
